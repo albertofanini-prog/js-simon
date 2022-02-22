7775fde491226002ed42c6f7155219873e4196a7
albertofanini-prog <albertofanini@outlook.com>
2022-02-22 14:58:11 +0100

    2nd Commit - Generare e comunicare 5 numeri random

--- script.js
+++ script.js
@@ -5,4 +5,21 @@ console.log('prova');
 //Alla fine dei 30 secondi
     //User deve inserire i numeri che ha visto precedentemente
     //Tramite prompt()
-//Il software comunica quanti e quali numrti sono stati indovinati
+//Il software comunica quanti e quali numeri sono stati indovinati
+
+
+//Creare array in cui inserire i numeri generati
+const numArray = [];
+
+//Generare 5 numeri random e inserirli nell'array
+while(numArray.length < 5){
+    //Generare numeri random da 1 a 100
+    const number = Math.floor(Math.random() * 100) + 1;
+
+    //Inserire i numeri nell'array
+    if(numArray.indexOf(number) === -1) numArray.push(number);
+}
+console.log(numArray);
+
+//Comunicare i numeri generati
+alert(`I numeri generati sono: ${numArray}`);
