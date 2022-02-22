98ad6c7db9c86f714daf2ad85c0cc6a7e211b78e
albertofanini-prog <albertofanini@outlook.com>
2022-02-22 14:46:53 +0100

    1st Commit - Upload files, spiegazione in italiano

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('prova');
+
+//Generare 5 numeri casuali e visualizzarli in pagina con un alert
+//Avviare un timer di 30 secondi dopo la chiusura dell'alert
+//Alla fine dei 30 secondi
+    //User deve inserire i numeri che ha visto precedentemente
+    //Tramite prompt()
+//Il software comunica quanti e quali numrti sono stati indovinati
